276c81ff1672626bfa40733f83c69e4994b64335
abnormalvis <2136555857@qq.com>
2025-11-22 00:36:18 +0800

    create pkg sentry_chassis_controller

---
+++ build/mimic_joint_controller/CMakeFiles/mimic_joint_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/idris/final_ws/devel/.private/mimic_joint_controller/lib/libmimic_joint_controller.pdb"
+  "/home/idris/final_ws/devel/.private/mimic_joint_controller/lib/libmimic_joint_controller.so"
+  "CMakeFiles/mimic_joint_controller.dir/src/mimic_joint_controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mimic_joint_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/robot_state_controller/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/idris/final_ws/devel/.private/robot_state_controller:$CMAKE_PREFIX_PATH"
+export PWD='/home/idris/final_ws/build/robot_state_controller'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/idris/final_ws/devel/.private/robot_state_controller/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/idris/final_ws/src/rm_controllers/robot_state_controller:$ROS_PACKAGE_PATH"
